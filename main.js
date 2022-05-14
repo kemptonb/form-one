d87f6476da51dcc03744621129e7fdaa28493bd8
bradley <bradleybkempton@gmail.com>
2022-05-13 19:48:43 -0700

    Revised JS errors on validation API.

--- main.js
+++ main.js
@@ -4,9 +4,13 @@
   //country
 
   const country = document.getElementById("myInput");
+  const autoList = document.getElementById("#myInputautocomplete-list");
 
-  country.addEventListener("input", function (event) {
-
+  country.addEventListener('keydown', function (event) {
+    //erase all input content on backspace
+    const key = event.key;
+      
+    //autocomplete
     function autocomplete(inp, arr) {
       /*the autocomplete function takes two arguments,
       the text field element and an array of possible autocompleted values:*/
@@ -51,31 +55,7 @@
         }
       });
 
-      /*execute a function presses a key on the keyboard:*/
-      inp.addEventListener("keydown", function (e) {
-        let x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-          /*If the arrow DOWN key is pressed,
-          increase the currentFocus variable:*/
-          currentFocus++;
-          /*and and make the current item more visible:*/
-          addActive(x);
-        } else if (e.keyCode == 38) { //up
-          /*If the arrow UP key is pressed,
-          decrease the currentFocus variable:*/
-          currentFocus--;
-          /*and and make the current item more visible:*/
-          addActive(x);
-        } else if (e.keyCode == 13) {
-          /*If the ENTER key is pressed, prevent the form from being submitted,*/
-          e.preventDefault();
-          if (currentFocus > -1) {
-            /*and simulate a click on the "active" item:*/
-            if (x) x[currentFocus].click();
-          }
-        }
-      });
+
 
       function addActive(x) {
         /*a function to classify an item as "active":*/
@@ -105,17 +85,28 @@
           }
         }
       }
+
       /*execute a function when someone clicks in the document:*/
       document.addEventListener("click", function (e) {
         closeAllLists(e.target);
       });
+
+      //erase all characters o backspace
+      if (key === "Backspace") {
+        country.value = "";
+        closeAllLists();
+      }
     }
+
+   
 
     /*An array containing all the country names in the world:*/
     var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre & Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts & Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks & Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
 
     /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
     autocomplete(document.getElementById("myInput"), countries);
+
+   
 
 
     //validate()
@@ -126,11 +117,9 @@
         country.setCustomValidity("");
         country.reportValidity();
         country.style.borderColor = "#ff8c00";
-        console.log(status);
       } else {
         country.setCustomValidity("");
         country.style.borderColor = "revert";
-        console.log(status);
       }
 
 
@@ -138,11 +127,12 @@
 
     validate();
 
-  });
+  });//end country autocomplete
 
   //email
 
   const email = document.getElementById("email");
+ 
 
   email.addEventListener("input", function (event) {
     if (email.validity.typeMismatch) {
@@ -173,28 +163,6 @@
       zip.style.borderColor = "#ff8c00";
     }
   });
-
-  //submit validate
-  const submit = document.getElementById("submit");
-  const inputs = document.getElementsByClassName("formInput");
-  
-  submit.addEventListener('click', () => {
-    
-  
-  });
-
-  // nameInput.addEventListener('input', () => {
-  //   nameInput.setCustomValidity('');
-  //   nameInput.checkValidity();
-  // });
-
-  // nameInput.addEventListener('invalid', () => {
-  //   if (nameInput.value === '') {
-  //     nameInput.setCustomValidity('Enter your username!');
-  //   } else {
-  //     nameInput.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
-  //   }
-  // });
 
 })();//end form-one
 
